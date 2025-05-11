626e0c8070ef08ed30612bb437b9aa720e1a43a2
adi1521 <apandey7998@gmail.com>
2025-05-11 06:22:46 +0000

    more content added in file

--- basics_javascript/script.js
+++ basics_javascript/script.js
@@ -1,1 +1,29 @@
 console.log("adityapandey")
+const AccountId = 23131
+let accountemail = "ad@hello.com"
+ accountemail = "hello@gmail.com"
+console.log(AccountId)
+console.log(accountemail)
+console.log("accountemail")
+
+// let accountEmail = "hitesh@google.com"
+// var accountPassword = "12345"
+// accountCity = "Jaipur"
+// let accountState;
+
+// // accountId = 2 // not allowed
+
+
+// accountEmail = "hc@hc.com"
+// accountPassword = "21212121"
+// accountCity = "Bengaluru"
+
+// console.log(accountId);
+
+// /*
+// Prefer not to use var
+// because of issue in block scope and functional scope
+// */
+
+
+// console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
