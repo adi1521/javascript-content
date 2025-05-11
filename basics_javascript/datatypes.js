58f99ba7feea1672d2460ccb5751a655dbf999ed
adi1521 <apandey7998@gmail.com>
2025-05-11 13:02:30 +0000

    datatypes

---
+++ basics_javascript/datatypes.js
@@ -0,0 +1,29 @@
+"use strict";//treat all js code as newer version
+
+// alert(3+3) // we are using nodejs,not browser
+console.log(3
+    +
+    5) //it works but code readability is not good so avoid it 
+    console.log("aditya")
+    let nam="aditya pandey"
+    let age=18
+    let isloggedIn= true
+    let state;
+
+//primitive data types:
+
+    //number=>2 to power 53
+    //bigint
+    //string=> ""
+    //boolean=> true/false
+    //null=> standalone value
+    //undefined=> value is not assigned
+    //symbol=> unique
+
+    //object
+
+    console.log(typeof nam)
+    console.log(typeof age)
+    console.log(typeof 18)
+    console.log(typeof undefined)
+    console.log(typeof null)
--- basics_javascript/script.js
+++ basics_javascript/script.js
@@ -2,9 +2,11 @@ console.log("adityapandey")
 const AccountId = 23131
 let accountemail = "ad@hello.com"
  accountemail = "hello@gmail.com"
+ AccountPassword="232@421#"
 console.log(AccountId)
 console.log(accountemail)
 console.log("accountemail")
+console.table([AccountId,accountemail,AccountPassword])
 
 // let accountEmail = "hitesh@google.com"
 // var accountPassword = "12345"
