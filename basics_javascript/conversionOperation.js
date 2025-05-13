c4c2f73d24c146d4ad8de04b2171b53dc5e41db7
adi1521 <apandey7998@gmail.com>
2025-05-13 09:46:51 +0000

    operation in js

--- basics_javascript/conversionOperation.js
+++ basics_javascript/conversionOperation.js
@@ -29,5 +29,36 @@ console.log(Instring)
 // let InNumber= Number(score)
 // console.log(typeof InNumber)
 // console.log(InNumber)
+
+// ******operations*********
+// console.log(2/3)
+// console.log(2*3)
+// console.log(4/2)
+// console.log(2%3)
   
+let str1 = "hello"
+let str2 = " aditya"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 3 + 2
+console.log(num1)
+
+let gameCounter = 200
+++gameCounter;
+console.log(gameCounter);
+
 
