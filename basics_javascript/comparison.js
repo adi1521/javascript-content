c9325c30253333101936eb47fe1551673a07e580
adi1521 <apandey7998@gmail.com>
2025-05-13 14:31:51 +0000

    comparison in javascript

---
+++ basics_javascript/comparison.js
@@ -0,0 +1,25 @@
+// console.log(2>1)
+// console.log(2!=1)
+// console.log("2"<1)
+// console.log("02">1)
+// console.log(null>0)
+// console.log(null<0);
+// console.log(null==0)
+// console.log(null>=0)
+// console.log(null<=0)
+//note: in comparison operator null value will be converted to number it will treated as 0. while in equality check it will not considered as a number that's why null==0 is false
+
+// console.log(undefined>0)
+// console.log(undefined<0)
+// console.log(undefined<=0)
+// console.log(undefined>=0)
+// console.log(undefined>null)
+// console.log(undefined==0)
+// console.log(undefined<=0)
+
+// // === (strict check which means it will check value and datatype as well)
+let x = "adi"
+console.log(2===2)
+console.log("3"===3)
+console.log("adi"===x)
+console.log("adi"==="adi")
