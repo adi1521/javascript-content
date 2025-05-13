c2040785788d3719611011fc9e1edd5eac711541
adi1521 <apandey7998@gmail.com>
2025-05-13 08:18:30 +0000

    conversionOperation file added

---
+++ basics_javascript/conversionOperation.js
@@ -0,0 +1,33 @@
+let score = ""
+console.log(typeof score)
+console.log(typeof(score))
+
+
+let instring= String(score)
+console.log(typeof instring)
+console.log(instring)
+
+let inboolean = Boolean(score)
+console.log(typeof inboolean)
+console.log(inboolean)
+
+score = true
+console.log(typeof score)
+
+let innumber= Number(score)
+console.log(typeof innumber)
+console.log(innumber)
+
+
+let Instring= String(score)
+console.log(typeof Instring)
+console.log(Instring)
+
+// score = undefined
+// console.log(typeof score)
+// console.log(score)
+// let InNumber= Number(score)
+// console.log(typeof InNumber)
+// console.log(InNumber)
+  
+
