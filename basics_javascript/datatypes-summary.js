d1b99df1b780bcc7e13c159701e0ceded061a4f8
adi1521 <apandey7998@gmail.com>
2025-06-05 14:33:08 +0530

    Update datatypes-summary.js

--- basics_javascript/datatypes-summary.js
+++ basics_javascript/datatypes-summary.js
@@ -6,7 +6,7 @@
 // array,objects,functions
 //  Primitive
 
-//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+//  7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
 const score = 100
 const scoreValue = 100.3
